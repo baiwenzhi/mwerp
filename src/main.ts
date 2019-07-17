4e1a41a2cac229359e54a1691c2b6699b8f50a18
baiwenzhi <wenzhibai@163.com>
2019-07-17 21:46:22 +0800

    wqweqwe

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+console.log(123123);
 if (environment.production) {
   enableProdMode();
 }
